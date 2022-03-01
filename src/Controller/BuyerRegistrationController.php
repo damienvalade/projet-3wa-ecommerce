672534551bfe040715a0ae46c2a4a297476f3a16
Xavier Laviron <xavier@norival.dev>
2022-03-01 11:52:01 +0100

    adds registration form for buyers

---
+++ src/Controller/BuyerRegistrationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Buyer;
+use App\Entity\User;
+use App\Form\BuyerRegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/buyer')]
+class BuyerRegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $buyer = new Buyer();
+        $form = $this->createForm(BuyerRegistrationFormType::class, $buyer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $buyer->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $buyer,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $buyer->setProfilePicture('');
+            $entityManager->persist($buyer);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_buyer');
+        }
+
+        return $this->render('buyer/registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -12,6 +13,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\DiscriminatorColumn(name: 'discr', type: 'string')]
 #[ORM\DiscriminatorMap(['user' => User::class, 'buyer' => Buyer::class, 'vendor' => Vendor::class])]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     CONST ROLE_BUYER = 'role_buyer';
